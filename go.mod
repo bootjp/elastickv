20d25fb45deadb2a8eff2b23f66b7d8c9a0ba13e
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2023-11-27 23:00:46 +0900

    impl raft fsm and grpc

--- go.mod
+++ go.mod
@@ -1,3 +1,25 @@
 module github.com/bootjp/elastickv
 
 go 1.21.0
+
+require (
+	github.com/hashicorp/raft v1.6.0
+	google.golang.org/grpc v1.59.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack/v2 v2.1.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+)
---
+++ kv/fsm.go
@@ -0,0 +1,91 @@
+package kv
+
+import (
+	"context"
+	"errors"
+	"io"
+	"sync"
+	"time"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/hashicorp/raft"
+
+	pb "github.com/bootjp/elastickv/proto"
+)
+
+type kv struct {
+	mtx sync.RWMutex
+	m   map[*[]byte][]byte
+}
+
+var _ raft.FSM = &kv{}
+
+func (f *kv) Apply(l *raft.Log) interface{} {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+
+	var req pb.PutRequest
+	if err := proto.Unmarshal(l.Data, &req); err != nil {
+		return err
+	}
+
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+
+	f.m[&req.Key] = req.Value
+
+	return nil
+}
+
+var ErrNotImplemented = errors.New("not implemented")
+
+func (f *kv) Snapshot() (raft.FSMSnapshot, error) {
+	return &snapshot{}, ErrNotImplemented
+}
+
+func (f *kv) Restore(r io.ReadCloser) error {
+	return ErrNotImplemented
+}
+
+type snapshot struct {
+	words []string
+}
+
+func (s *snapshot) Persist(sink raft.SnapshotSink) error {
+	return ErrNotImplemented
+}
+
+func (s *snapshot) Release() {
+}
+
+type rpcInterface struct {
+	store *kv
+	raft  *raft.Raft
+}
+
+var ErrRetrieable = errors.New("retrievable error")
+
+func (r rpcInterface) Put(ctx context.Context, req *pb.PutRequest) (*pb.PutResponse, error) {
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	f := r.raft.Apply(b, time.Second)
+	if err := f.Error(); err != nil {
+		return nil, ErrRetrieable
+	}
+	return &pb.PutResponse{
+		CommitIndex: f.Index(),
+	}, nil
+}
+
+func (r rpcInterface) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
+	r.store.mtx.RLock()
+	defer r.store.mtx.RUnlock()
+	return &pb.GetResponse{
+		ReadAtIndex: r.raft.AppliedIndex(),
+		Value:       r.store.m[&req.Key],
+	}, nil
+}
