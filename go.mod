346c20039ab011f9da783838400874ee288e69bb
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2023-12-08 23:33:05 +0900

    wip [skip ci]

--- go.mod
+++ go.mod
@@ -10,7 +10,8 @@ require (
 	github.com/cockroachdb/errors v1.11.1
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	github.com/hashicorp/raft v1.6.0
-	github.com/hashicorp/raft-boltdb v0.0.0-20230125174641-2a8082862702
+	github.com/lni/dragonboat/v4 v4.0.0-20230922120309-657304aedc15
+	github.com/lni/goutils v1.3.1-0.20220604063047-388d67b4dbc4
 	github.com/spaolacci/murmur3 v1.1.0
 	github.com/stretchr/testify v1.8.4
 	go.etcd.io/bbolt v1.3.8
@@ -19,29 +20,47 @@ require (
 )
 
 require (
+	github.com/DataDog/zstd v1.5.2 // indirect
+	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/VictoriaMetrics/metrics v1.18.1 // indirect
 	github.com/armon/go-metrics v0.4.1 // indirect
-	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cockroachdb/logtags v0.0.0-20230118201751-21c54148d20b // indirect
+	github.com/cockroachdb/pebble v0.0.0-20221207173255-0f086d933dac // indirect
 	github.com/cockroachdb/redact v1.1.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/getsentry/sentry-go v0.18.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-hclog v1.5.0 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-msgpack v1.1.5 // indirect
 	github.com/hashicorp/go-msgpack/v2 v2.1.1 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.0 // indirect
 	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/hashicorp/memberlist v0.3.1 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/lni/vfs v0.2.1-0.20220616104132-8852fd867376 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/miekg/dns v1.1.26 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	github.com/valyala/fastrand v1.1.0 // indirect
+	github.com/valyala/histogram v1.2.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
 	golang.org/x/net v0.16.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- main.go
+++ main.go
@@ -1,115 +1,198 @@
+// Copyright 2017,2018 Lei Ni (nilei81@gmail.com).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+multigroup is an example program for dragonboat demonstrating how multiple
+raft groups can be used in an user application.
+*/
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
-	"log"
-	"net"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"runtime"
+	"strings"
+	"syscall"
+	"time"
 
-	"github.com/Jille/raft-grpc-leader-rpc/leaderhealth"
-	transport "github.com/Jille/raft-grpc-transport"
-	"github.com/Jille/raftadmin"
 	"github.com/bootjp/elastickv/kv"
-	pb "github.com/bootjp/elastickv/proto"
-	"github.com/cockroachdb/errors"
-	"github.com/hashicorp/raft"
-	boltdb "github.com/hashicorp/raft-boltdb"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/reflection"
+
+	"github.com/lni/dragonboat/v4"
+	"github.com/lni/dragonboat/v4/config"
+	"github.com/lni/dragonboat/v4/logger"
+	"github.com/lni/goutils/syncutil"
+)
+
+const (
+	// we use two raft groups in this example, they are identified by the cluster
+	// ID values below
+	shardID1 uint64 = 100
+	shardID2 uint64 = 101
 )
 
 var (
-	myAddr = flag.String("address", "localhost:50051", "TCP host+port for this node")
-	raftId = flag.String("raft_id", "", "Node id used by Raft")
-
-	raftDir       = flag.String("raft_data_dir", "data/", "Raft data dir")
-	raftBootstrap = flag.Bool("raft_bootstrap", false, "Whether to bootstrap the Raft cluster")
+	// initial nodes count is three, their addresses are also fixed
+	// this is for simplicity
+	addresses = []string{
+		"localhost:63001",
+		"localhost:63002",
+		"localhost:63003",
+	}
 )
 
 func main() {
+	replicaID := flag.Int("nodeid", 1, "ReplicaID to use")
 	flag.Parse()
-
-	if *raftId == "" {
-		log.Fatalf("flag --raft_id is required")
+	if *replicaID > 3 || *replicaID < 1 {
+		fmt.Fprintf(os.Stderr, "invalid nodeid %d, it must be 1, 2 or 3", *replicaID)
+		os.Exit(1)
 	}
-
-	ctx := context.Background()
-	_, port, err := net.SplitHostPort(*myAddr)
+	// https://github.com/golang/go/issues/17393
+	if runtime.GOOS == "darwin" {
+		signal.Ignore(syscall.Signal(0xd))
+	}
+	initialMembers := make(map[uint64]string)
+	for idx, v := range addresses {
+		// key is the ReplicaID, ReplicaID is not allowed to be 0
+		// value is the raft address
+		initialMembers[uint64(idx+1)] = v
+	}
+	nodeAddr := initialMembers[uint64(*replicaID)]
+	fmt.Fprintf(os.Stdout, "node address: %s\n", nodeAddr)
+	// change the log verbosity
+	logger.GetLogger("raft").SetLevel(logger.ERROR)
+	logger.GetLogger("rsm").SetLevel(logger.WARNING)
+	logger.GetLogger("transport").SetLevel(logger.WARNING)
+	logger.GetLogger("grpc").SetLevel(logger.WARNING)
+	// config for raft
+	// note the ShardID value is not specified here
+	rc := config.Config{
+		ReplicaID:          uint64(*replicaID),
+		ElectionRTT:        5,
+		HeartbeatRTT:       1,
+		CheckQuorum:        true,
+		SnapshotEntries:    10,
+		CompactionOverhead: 5,
+	}
+	datadir := filepath.Join(
+		"example-data",
+		"multigroup-data",
+		fmt.Sprintf("node%d", *replicaID))
+	// config for the nodehost
+	// by default, insecure transport is used, you can choose to use Mutual TLS
+	// Authentication to authenticate both servers and clients. To use Mutual
+	// TLS Authentication, set the MutualTLS field in NodeHostConfig to true, set
+	// the CAFile, CertFile and KeyFile fields to point to the path of your CA
+	// file, certificate and key files.
+	// by default, TCP based RPC module is used, set the RaftRPCFactory field in
+	// NodeHostConfig to rpc.NewRaftGRPC (github.com/lni/dragonboat/plugin/rpc) to
+	// use gRPC based transport. To use gRPC based RPC module, you need to install
+	// the gRPC library first -
+	//
+	// $ go get -u google.golang.org/grpc
+	//
+	nhc := config.NodeHostConfig{
+		WALDir:         datadir,
+		NodeHostDir:    datadir,
+		RTTMillisecond: 200,
+		RaftAddress:    nodeAddr,
+		// RaftRPCFactory: rpc.NewRaftGRPC,
+	}
+	// create a NodeHost instance. it is a facade interface allowing access to
+	// all functionalities provided by dragonboat.
+	nh, err := dragonboat.NewNodeHost(nhc)
 	if err != nil {
-		log.Fatalf("failed to parse local address (%q): %v", *myAddr, err)
-	}
-	sock, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	store := kv.NewMemoryStore()
-	kvFSM := kv.NewKvFSM(store)
-	r, tm, err := NewRaft(ctx, *raftId, *myAddr, kvFSM)
-	if err != nil {
-		log.Fatalf("failed to start raft: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterRawKVServer(s, kv.NewGRPCServer(kvFSM, store, r))
-	tm.Register(s)
-	leaderhealth.Setup(r, s, []string{"RawKV", "Example"})
-	raftadmin.Register(s, r)
-	reflection.Register(s)
-	if err := s.Serve(sock); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
-
-const snapshotRetainCount = 3
-
-func NewRaft(_ context.Context, myID, myAddress string, fsm raft.FSM) (*raft.Raft, *transport.Manager, error) {
-	c := raft.DefaultConfig()
-	c.LocalID = raft.ServerID(myID)
-
-	baseDir := filepath.Join(*raftDir, myID)
-
-	ldb, err := boltdb.NewBoltStore(filepath.Join(baseDir, "logs.dat"))
-	if err != nil {
-		return nil, nil, errors.WithStack(err)
+		panic(err)
 	}
 
-	sdb, err := boltdb.NewBoltStore(filepath.Join(baseDir, "stable.dat"))
-	if err != nil {
-		return nil, nil, errors.WithStack(err)
+	defer nh.Close()
+	// start the first cluster
+	// we use ExampleStateMachine as the IStateMachine for this cluster, its
+	// behaviour is identical to the one used in the Hello World example.
+	rc.ShardID = shardID1
+	if err := nh.StartReplica(initialMembers, false, kv.NewExampleStateMachine, rc); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to add cluster, %v\n", err)
+		os.Exit(1)
 	}
-
-	fss, err := raft.NewFileSnapshotStore(baseDir, snapshotRetainCount, os.Stderr)
-	if err != nil {
-		return nil, nil, errors.WithStack(err)
+	// start the second cluster
+	// we use SecondStateMachine as the IStateMachine for the second cluster
+	rc.ShardID = shardID2
+	if err := nh.StartReplica(initialMembers, false, kv.NewExampleStateMachine, rc); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to add cluster, %v\n", err)
+		os.Exit(1)
 	}
-
-	tm := transport.New(raft.ServerAddress(myAddress), []grpc.DialOption{
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	raftStopper := syncutil.NewStopper()
+	consoleStopper := syncutil.NewStopper()
+	ch := make(chan string, 16)
+	consoleStopper.RunWorker(func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			s, err := reader.ReadString('\n')
+			if err != nil {
+				close(ch)
+				return
+			}
+			if s == "exit\n" {
+				raftStopper.Stop()
+				// no data will be lost/corrupted if nodehost.Stop() is not called
+				nh.Close()
+				return
+			}
+			ch <- s
+		}
 	})
-
-	r, err := raft.NewRaft(c, fsm, ldb, sdb, fss, tm.Transport())
-	if err != nil {
-		return nil, nil, errors.WithStack(err)
-	}
-
-	if *raftBootstrap {
-		cfg := raft.Configuration{
-			Servers: []raft.Server{
-				{
-					Suffrage: raft.Voter,
-					ID:       raft.ServerID(myID),
-					Address:  raft.ServerAddress(myAddress),
-				},
-			},
+	raftStopper.RunWorker(func() {
+		// use NO-OP client session here
+		// check the example in godoc to see how to use a regular client session
+		cs1 := nh.GetNoOPSession(shardID1)
+		cs2 := nh.GetNoOPSession(shardID2)
+		for {
+			select {
+			case v, ok := <-ch:
+				if !ok {
+					return
+				}
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+				// remove the \n char
+				msg := strings.Replace(strings.TrimSpace(v), "\n", "", 1)
+				var err error
+				// In this example, the strategy on how data is sharded across different
+				// Raft groups is based on whether the input message ends with a "?".
+				// In your application, you are free to choose strategies suitable for
+				// your application.
+				if strings.HasSuffix(msg, "?") {
+					// user message ends with "?", make a proposal to update the second
+					// raft group
+					_, err = nh.SyncPropose(ctx, cs2, []byte(msg))
+				} else {
+					// message not ends with "?", make a proposal to update the first
+					// raft group
+					_, err = nh.SyncPropose(ctx, cs1, []byte(msg))
+				}
+				cancel()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "SyncPropose returned error %v\n", err)
+				}
+			case <-raftStopper.ShouldStop():
+				return
+			}
 		}
-		f := r.BootstrapCluster(cfg)
-		if err := f.Error(); err != nil {
-			return nil, nil, errors.WithStack(err)
-		}
-	}
-
-	return r, tm, nil
+	})
+	raftStopper.Wait()
 }
