2567c8765d8c904df01017d6ec520ca8598225b8
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2023-12-10 12:04:39 +0900

    fix test

--- kv/bolt_store_test.go
+++ kv/bolt_store_test.go
@@ -16,26 +16,26 @@ func mustStore(store Store, err error) Store {
 	return store
 }
 
-func TestStore(t *testing.T) {
+func TestBoltStore(t *testing.T) {
 	ctx := context.Background()
 	t.Parallel()
 	st := mustStore(NewBoltStore(os.TempDir() + "/bolt.db"))
 
-	t.Run(st.Name(), func(t *testing.T) {
-		for i := 0; i < 99999; i++ {
-			go func(i int) {
-				key := []byte("foo" + strconv.Itoa(i))
-				err := st.Put(ctx, key, []byte("bar"))
-				assert.NoError(t, err)
+	for i := 0; i < 99999; i++ {
+		go func(i int) {
+			key := []byte("foo" + strconv.Itoa(i))
+			err := st.Put(ctx, key, []byte("bar"))
+			assert.NoError(t, err)
 
-				res, err := st.Get(ctx, key)
-				assert.NoError(t, err)
-				assert.Equal(t, []byte("bar"), res)
+			res, err := st.Get(ctx, key)
+			assert.NoError(t, err)
+			assert.Equal(t, []byte("bar"), res)
 
-				assert.NoError(t, st.Delete(ctx, key))
-				// bolt store does not support NotFound
-				assert.Nil(t, res)
-			}(i)
-		}
-	})
+			assert.NoError(t, st.Delete(ctx, key))
+			// bolt store does not support NotFound
+			res, err = st.Get(ctx, []byte("aaaaaa"))
+			assert.NoError(t, err)
+			assert.Nil(t, res)
+		}(i)
+	}
 }
