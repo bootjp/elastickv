53b0c2d03d44bd7c705cdbf6488748d7f7251c8d
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2023-12-25 13:45:56 +0900

    Refactor transaction commit logic to return the commit index and handle errors properly

--- kv/transcoder.go
+++ kv/transcoder.go
@@ -1,18 +1,22 @@
 package kv
 
+// OP is an operation type.
 type OP int
 
+// Operation types.
 const (
 	Put OP = iota
 	Del
 )
 
+// Elem is an element of a transaction.
 type Elem[T OP] struct {
 	Op    T
 	Key   []byte
 	Value []byte
 }
 
+// OperationGroup is a group of operations that should be executed atomically.
 type OperationGroup[T OP] struct {
 	Elems []*Elem[T]
 	IsTxn bool
