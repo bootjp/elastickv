ae7994a0d13e0366160698456587d709c5c2df45
Yoshiaki Ueda (bootjp) <contact@bootjp.me>
2026-04-01 02:19:13 +0900

    store/proxy: fix lint findings

--- store/mvcc_store.go
+++ store/mvcc_store.go
@@ -902,10 +902,9 @@ func compactKeepIndex(versions []VersionedValue, minTS uint64) int {
 		return -1
 	}
 	keepIdx := -1
-	for i := len(versions); i > 0; i-- {
-		if versions[i-1].TS <= minTS {
-			keepIdx = i - 1
-			break
+	for i, version := range versions {
+		if version.TS <= minTS {
+			keepIdx = i
 		}
 	}
 	if keepIdx <= 0 {
