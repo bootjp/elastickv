e34c497bc5d70c20da2b83ca97718f05e63f5a98
bootjp <contact@bootjp.me>
2026-07-08 01:31:25 +0900

    Share no-follow snapshot file opens

--- internal/backup/archive_open_read.go
+++ internal/backup/archive_open_read.go
@@ -1,23 +1,20 @@
-//go:build unix
-
 package backup
 
 import (
 	"os"
-	"syscall"
 
+	"github.com/bootjp/elastickv/internal/pathsafety"
 	"github.com/cockroachdb/errors"
 )
 
 func openArchiveRegularForRead(path string) (*os.File, os.FileInfo, error) {
-	fd, err := syscall.Open(path, syscall.O_RDONLY|syscall.O_NOFOLLOW|syscall.O_NONBLOCK, 0) //nolint:gosec // path came from WalkDir under the verified dump root
+	f, err := pathsafety.OpenNoFollowRead(path)
 	if err != nil {
-		if errors.Is(err, syscall.ELOOP) {
+		if pathsafety.IsSymlink(err) {
 			return nil, nil, errors.Wrapf(ErrArchiveNonRegular, "%s is a symlink", path)
 		}
 		return nil, nil, errors.WithStack(err)
 	}
-	f := os.NewFile(uintptr(fd), path)
 	info, err := f.Stat()
 	if err != nil {
 		_ = f.Close()
